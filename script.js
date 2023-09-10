eb7c337f8b40ad260e998171a8d356d1910052e0
Cybelle Bem <cybellegbem@gmail.com>
2023-09-10 19:51:51 -0300

    alterações na funcionalidade

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const CONTRAST_THRESHOLD_AA = 4.5;
+const CONTRAST_THRESHOLD_AAA = 7;
+const CONTRAST_THRESHOLD_AA_18PT = 3;
+const CONTRAST_THRESHOLD_AAA_18PT = 4.5;
+
 function normalizeHexValue(hex) {
     hex = hex.trim();
     if (hex.startsWith('#')) {
@@ -8,6 +13,7 @@ function normalizeHexValue(hex) {
 }
 
 function hexToRgb(hex) {
+    hex = normalizeHexValue(hex);
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
     hex = hex.replace(shorthandRegex, function(m, r, g, b) {
         return r + r + g + g + b + b;
@@ -42,14 +48,10 @@ function updateContrastRatio() {
     const backgroundHexInput = document.getElementById('background-hex');
     const textHexInput = document.getElementById('text-hex');
     const customCardBodies = document.querySelectorAll('.custom-card-body');
+    const contrastRatioDisplay = document.getElementById('contrast-ratio');
 
-    const backgroundColor = backgroundHexInput.value ?
-        `#${normalizeHexValue(backgroundHexInput.value)}` :
-        document.getElementById('background-color').value;
-
-    const textColor = textHexInput.value ?
-        `#${normalizeHexValue(textHexInput.value)}` :
-        document.getElementById('text-color').value;
+    const backgroundColor = backgroundHexInput.value ? `#${normalizeHexValue(backgroundHexInput.value)}` : document.getElementById('background-color').value;
+    const textColor = textHexInput.value ? `#${normalizeHexValue(textHexInput.value)}` : document.getElementById('text-color').value;
 
     customCardBodies.forEach(cardBody => {
         cardBody.style.backgroundColor = backgroundColor;
@@ -60,23 +62,12 @@ function updateContrastRatio() {
     const textRgb = hexToRgb(textColor);
 
     const contrastRatio = calculateContrastRatio(backgroundRgb, textRgb);
-
-    const contrastRatioDisplay = document.getElementById('contrast-ratio');
     contrastRatioDisplay.innerText = `RATIO: ${contrastRatio}`;
 
-    const sampleText12 = document.getElementById('sample-text-12');
-    const sampleText18 = document.getElementById('sample-text-18');
+    updateContrastCircles(contrastRatio);
+}
 
-    sampleText12.style.backgroundColor = backgroundColor;
-    sampleText12.style.color = textColor;
-    sampleText18.style.backgroundColor = backgroundColor;
-    sampleText18.style.color = textColor;
-
-    const isAAMeeting = contrastRatio >= 4.5;
-    const isAAAMeeting = contrastRatio >= 7;
-    const isAA18ptMeeting = contrastRatio >= 3;
-    const isAAA18ptMeeting = contrastRatio >= 4.5;
-
+function updateContrastCircles(contrastRatio) {
     const circleAA = document.getElementById('circleAA');
     const circleAAA = document.getElementById('circleAAA');
     const circleAA18PT = document.getElementById('circleAA18PT');
@@ -87,38 +78,128 @@ function updateContrastRatio() {
     const iconAA18PT = document.getElementById('iconAA18PT');
     const iconAAA18PT = document.getElementById('iconAAA18PT');
 
-    circleAA.style.backgroundColor = isAAMeeting ? 'green' : 'red';
-    circleAAA.style.backgroundColor = isAAAMeeting ? 'green' : 'red';
-    circleAA18PT.style.backgroundColor = isAA18ptMeeting ? 'green' : 'red';
-    circleAAA18PT.style.backgroundColor = isAAA18ptMeeting ? 'green' : 'red';
+    const isAAMeeting = contrastRatio >= CONTRAST_THRESHOLD_AA;
+    const isAAAMeeting = contrastRatio >= CONTRAST_THRESHOLD_AAA;
+    const isAA18ptMeeting = contrastRatio >= CONTRAST_THRESHOLD_AA_18PT;
+    const isAAA18ptMeeting = contrastRatio >= CONTRAST_THRESHOLD_AAA_18PT;
 
-    iconAA.innerText = isAAMeeting ? '✓' : '✕';
-    iconAAA.innerText = isAAAMeeting ? '✓' : '✕';
-    iconAA18PT.innerText = isAA18ptMeeting ? '✓' : '✕';
-    iconAAA18PT.innerText = isAAA18ptMeeting ? '✓' : '✕';
+    updateCircle(circleAA, isAAMeeting);
+    updateCircle(circleAAA, isAAAMeeting);
+    updateCircle(circleAA18PT, isAA18ptMeeting);
+    updateCircle(circleAAA18PT, isAAA18ptMeeting);
+
+    updateIcon(iconAA, isAAMeeting);
+    updateIcon(iconAAA, isAAAMeeting);
+    updateIcon(iconAA18PT, isAA18ptMeeting);
+    updateIcon(iconAAA18PT, isAAA18ptMeeting);
+}
+
+function updateCircle(circle, status) {
+    circle.style.backgroundColor = status ? 'green' : 'red';
+}
+
+function updateIcon(icon, status) {
+    icon.innerText = status ? '✓' : '✕';
+}
+
+function addToHistory(status, contrastRatio, backgroundColor, textColor) {
+    const historyTableBody = document.getElementById('history-table-body');
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td><div class="circle2 ${getStatusClass(contrastRatio, 4.5)}">AA <br>${getStatusIcon(contrastRatio, 4.5)}</div></td>
+        <td><div class="circle2 ${getStatusClass(contrastRatio, 7)}">AAA <br>${getStatusIcon(contrastRatio, 7)}</div></td>
+        <td><div class="circle2 ${getStatusClass(contrastRatio, 3)}">AA18+ <br>${getStatusIcon(contrastRatio, 3)}</div></td>
+        <td><div class="circle2 ${getStatusClass(contrastRatio, 4.5)}">AAA18+ <br>${getStatusIcon(contrastRatio, 4.5)}</div></td>
+        <td>${contrastRatio}</td>
+        <td class="sample"></td>
+        <td>${backgroundColor}</td>
+        <td>${textColor}</td>
+        <td><button class="delete-button">Excluir</button></td>
+    `;
+
+    const fontSize12 = '12px';
+    const fontSize18 = '18px';
+
+    const sampleContainer = newRow.querySelector('.sample');
+    const sample12 = createContrastSample(backgroundColor, textColor, fontSize12);
+    const sample18 = createContrastSample(backgroundColor, textColor, fontSize18);
+
+    sampleContainer.appendChild(sample12);
+    sampleContainer.appendChild(sample18);
+
+    historyTableBody.appendChild(newRow);
+
+    const deleteButton = newRow.querySelector('.delete-button');
+    deleteButton.addEventListener('click', function() {
+        historyTableBody.removeChild(newRow);
+        removeFromHistory(newRow);
+    });
+
+    addToHistoryCookies(status, contrastRatio, backgroundColor, textColor);
+}
+
+function getStatusClass(contrastRatio, threshold) {
+    return contrastRatio >= threshold ? 'green' : 'red';
+}
+
+function getStatusIcon(contrastRatio, threshold) {
+    return contrastRatio >= threshold ? '✓' : '✕';
+}
+
+function saveHistoryToCookies(history) {
+    const historyJSON = JSON.stringify(history);
+    localStorage.setItem('history', historyJSON);
+}
+
+function retrieveHistoryFromCookies() {
+    const historyJSON = localStorage.getItem('history');
+    return historyJSON ? JSON.parse(historyJSON) : [];
+}
+
+function displayHistoryOnPage() {
+    const history = retrieveHistoryFromCookies();
+    const historyTableBody = document.getElementById('history-table-body');
+    for (const item of history) {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td><div class="circle2 ${getStatusClass(item.contrastRatio, 4.5)}">AA <br>${getStatusIcon(item.contrastRatio, 4.5)}</div></td>
+            <td><div class="circle2 ${getStatusClass(item.contrastRatio, 7)}">AAA <br>${getStatusIcon(item.contrastRatio, 7)}</div></td>
+            <td><div class="circle2 ${getStatusClass(item.contrastRatio, 3)}">AA18+ <br>${getStatusIcon(item.contrastRatio, 3)}</div></td>
+            <td><div class="circle2 ${getStatusClass(item.contrastRatio, 4.5)}">AAA18+ <br>${getStatusIcon(item.contrastRatio, 4.5)}</div></td>
+            <td>${item.contrastRatio}</td>
+            <td class="sample"></td>
+            <td>${item.backgroundColor}</td>
+            <td>${item.textColor}</td>
+            <td><button class="delete-button">Excluir</button></td>
+        `;
+
+        const sampleContainer = newRow.querySelector('.sample');
+        sampleContainer.innerHTML = item.sample12 + '<br>' + item.sample18;
+
+        historyTableBody.appendChild(newRow);
+
+        const deleteButton = newRow.querySelector('.delete-button');
+        deleteButton.addEventListener('click', function() {
+            const index = history.indexOf(item);
+            if (index !== -1) {
+                history.splice(index, 1);
+                saveHistoryToCookies(history);
+            }
+            newRow.remove();
+        });
+    }
 }
 
 document.getElementById('background-color').addEventListener('change', updateContrastRatio);
-
 document.getElementById('text-color').addEventListener('change', updateContrastRatio);
-
 document.getElementById('background-hex').addEventListener('input', updateContrastRatio);
-
 document.getElementById('text-hex').addEventListener('input', updateContrastRatio);
 
-const backgroundColorPicker = document.getElementById('background-color');
-const backgroundHexInput = document.getElementById('background-hex');
-const textColorPicker = document.getElementById('text-color');
-const textHexInput = document.getElementById('text-hex');
-
-backgroundColorPicker.addEventListener('input', function(e) {
-    const hexColor = e.target.value;
-    backgroundHexInput.value = hexColor;
+document.getElementById('background-color').addEventListener('input', function(e) {
+    document.getElementById('background-hex').value = e.target.value;
 });
-
-textColorPicker.addEventListener('input', function(e) {
-    const hexColor = e.target.value;
-    textHexInput.value = hexColor;
+document.getElementById('text-color').addEventListener('input', function(e) {
+    document.getElementById('text-hex').value = e.target.value;
 });
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -135,55 +216,98 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-function addToHistory(status, contrastRatio, backgroundColor, textColor) {
-    const historyTableBody = document.getElementById('history-table-body');
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
-    <td>
-      <div class="circle2 ${contrastRatio >= 4.5 ? 'green' : 'red'}">
-        AA <br> ${contrastRatio >= 4.5 ? '✓' : '✕'}
-      </div>
-    </td>
-    <td>
-      <div class="circle2 ${contrastRatio >= 7 ? 'green' : 'red'}">
-      AAA <br> ${contrastRatio >= 7 ? '✓' : '✕'}
-      </div>
-    </td>
-    <td>
-      <div class="circle2 ${contrastRatio >= 3 ? 'green' : 'red'}">
-      AA18+ <br>${contrastRatio >= 3 ? '✓' : '✕'}
-      </div>
-    </td>
-    <td>
-      <div class="circle2 ${contrastRatio >= 4.5 ? 'green' : 'red'}">
-      AAA18+ <br> ${contrastRatio >= 4.5 ? '✓' : '✕'}
-      </div>
-    </td>
-        <td>${contrastRatio}</td>
-        <td>${backgroundColor}</td>
-        <td>${textColor}</td>
-        <td><button class="delete-button">Excluir</button></td>
-    `;
-
-    historyTableBody.appendChild(newRow);
-
-    const deleteButton = newRow.querySelector('.delete-button');
-    deleteButton.addEventListener('click', function() {
-        newRow.remove();
-    });
-}
-
 const addToHistoryButton = document.getElementById('add-to-history');
 addToHistoryButton.addEventListener('click', function() {
     const contrastRatioDisplay = document.getElementById('contrast-ratio');
     const backgroundColorPicker = document.getElementById('background-color');
     const textColorPicker = document.getElementById('text-color');
 
-    const contrastRatio = contrastRatioDisplay.textContent.split(':')[1].trim();
+    const contrastRatio = parseFloat(contrastRatioDisplay.textContent.split(':')[1].trim());
     const backgroundColor = backgroundColorPicker.value;
     const textColor = textColorPicker.value;
 
-    const status = parseFloat(contrastRatio) >= 4.5 ? 'Aprovado' : 'Reprovado';
+    const status = contrastRatio >= 4.5 ? 'Aprovado' : 'Reprovado';
+
+    const history = retrieveHistoryFromCookies();
+    history.push({
+        status,
+        contrastRatio,
+        backgroundColor,
+        textColor,
+        sample12: getSampleHTML(backgroundColor, textColor, '12px'),
+        sample18: getSampleHTML(backgroundColor, textColor, '18px'),
+    });
+    saveHistoryToCookies(history);
 
     addToHistory(status, contrastRatio, backgroundColor, textColor);
 });
+
+function createContrastSample(background, text, fontSize) {
+    const sample = document.createElement('div');
+    sample.style.backgroundColor = background;
+    sample.style.color = text;
+    sample.style.fontSize = fontSize;
+    sample.innerHTML = 'exemplo de texto<br>EXEMPLO DE TEXTO';
+    return sample;
+}
+
+function getSampleHTML(background, text, fontSize) {
+    const sample = createContrastSample(background, text, fontSize);
+    return sample.outerHTML;
+}
+
+document.addEventListener("DOMContentLoaded", displayHistoryOnPage);
+
+function removeFromHistory(row) {
+    const history = retrieveHistoryFromCookies();
+    const index = Array.from(row.parentNode.children).indexOf(row);
+    if (index !== -1) {
+        history.splice(index, 1);
+        saveHistoryToCookies(history);
+    }
+    row.remove();
+}
+
+function calculateContrastWithDefault() {
+    const backgroundHexInput = document.getElementById('background-hex');
+    const textHexInput = document.getElementById('text-hex');
+    const backgroundColor = `#${normalizeHexValue(backgroundHexInput.value) || document.getElementById('background-color').value}`;
+    const textColor = `#${normalizeHexValue(textHexInput.value) || document.getElementById('text-color').value}`;
+    const backgroundRgb = hexToRgb(backgroundColor);
+    const textRgb = hexToRgb(textColor);
+    const contrastRatio = calculateContrastRatio(backgroundRgb, textRgb);
+    const contrastRatioDisplay = document.getElementById('contrast-ratio');
+    contrastRatioDisplay.innerText = `RATIO: ${contrastRatio}`;
+    
+    updateContrastCircles(contrastRatio);
+}
+
+function updateCardColor() {
+    const backgroundHexInput = document.getElementById('background-hex');
+    const textHexInput = document.getElementById('text-hex');
+    const customCardBodies = document.querySelectorAll('.custom-card-body');
+
+    const backgroundColor = backgroundHexInput.value ? `#${normalizeHexValue(backgroundHexInput.value)}` : document.getElementById('background-color').value;
+    const textColor = textHexInput.value ? `#${normalizeHexValue(textHexInput.value)}` : document.getElementById('text-color').value;
+
+    customCardBodies.forEach(cardBody => {
+        cardBody.style.backgroundColor = backgroundColor;
+        cardBody.style.color = textColor;
+    });
+}
+
+document.getElementById('background-color').addEventListener('change', updateCardColor);
+document.getElementById('text-color').addEventListener('change', updateCardColor);
+document.getElementById('background-hex').addEventListener('input', updateCardColor);
+document.getElementById('text-hex').addEventListener('input', updateCardColor);
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateCardColor();
+});
+
+document.getElementById('background-color').addEventListener('change', calculateContrastWithDefault);
+document.getElementById('text-color').addEventListener('change', calculateContrastWithDefault);
+document.getElementById('background-hex').addEventListener('input', calculateContrastWithDefault);
+document.getElementById('text-hex').addEventListener('input', calculateContrastWithDefault);
+
+document.addEventListener("DOMContentLoaded", calculateContrastWithDefault);
